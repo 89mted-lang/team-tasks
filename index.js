b3c9d0f659e975378a563f6ba2ea6afd3a62d679
mted <89m.ted@gmail.com>
2025-08-20 17:42:33 +0300

    feat: серверное хранение задач

--- index.js
+++ index.js
@@ -19,3 +19,27 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
 });
+const fs = require('fs');
+const TASKS_FILE = path.join(__dirname, 'tasks.json');
+
+// Получить все задачи
+app.get('/api/tasks', (req, res) => {
+  fs.readFile(TASKS_FILE, 'utf8', (err, data) => {
+    if (err) return res.json([]);
+    res.json(JSON.parse(data));
+  });
+});
+
+// Добавить новую задачу
+app.post('/api/tasks', express.json(), (req, res) => {
+  fs.readFile(TASKS_FILE, 'utf8', (err, data) => {
+    let tasks = [];
+    if (!err && data) tasks = JSON.parse(data);
+    const newTask = req.body;
+    newTask.id = tasks.length ? Math.max(...tasks.map(t => t.id)) + 1 : 1;
+    tasks.push(newTask);
+    fs.writeFile(TASKS_FILE, JSON.stringify(tasks, null, 2), () => {
+      res.json(newTask);
+    });
+  });
+});
